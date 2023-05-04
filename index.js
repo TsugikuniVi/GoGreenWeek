0bee082cc3c6bb0c997023c7d057a60aae02b115
Saayuj Sajith <saayuj1015@gmail.com>
2023-05-04 23:09:21 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const questions = require('./questions.js');
+
+app.use(express.static("public"));
+
+app.get(['/', '/easy','/medium','/hard'], (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get("/question/:difficulty", (req, res) => {
+  const { difficulty } = req.params;
+  const questionsByDifficulty = questions[difficulty];
+  const randomQuestion = questionsByDifficulty[Math.floor(Math.random() * questionsByDifficulty.length)];
+  res.send(randomQuestion);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ questions.js
@@ -0,0 +1,79 @@
+const questions = {
+  easy: [
+    {
+      question: "What is the value of x in the equation 3x = 18?",
+      answer: "6",
+    },
+    {
+      question: "Solve for x: 5x - 7 = 18",
+      answer: "5",
+    },
+    {
+      question: "Find the value of x if 2x + 3 = 11",
+      answer: "4",
+    },
+    {
+      question: "Solve for x: 8x - 12 = 20 + 2x",
+      answer: "2",
+    },
+    {
+      question: "What is the value of x in the equation 7x - 5 = 12?",
+      answer: "17/7",
+    },
+  ],
+  medium: [
+    {
+      question: "Solve for x: 2(x + 3) = 16",
+      answer: "5",
+    },
+    {
+      question: "Find the value of x if 4x - 9 = 3x + 6",
+      answer: "15",
+    },
+    {
+      question: "Solve for x: 3(2x - 1) = 15",
+      answer: "3",
+    },
+    {
+      question: "What is the value of x in the equation 5x + 7 = 3x - 1?",
+      answer: "-2",
+    },
+    {
+      question: "Solve for x: 2x + 5 = x - 7",
+      answer: "-12",
+    },
+    {
+      question: "Find the value of x if 3(x + 2) = 27",
+      answer: "7",
+    },
+    {
+      question: "Solve for x: 4x + 9 = 5(x - 2)",
+      answer: "23",
+    },
+  ],
+  hard: [
+    {
+      question: "What is the value of x in the equation 2x - 3 = 7x + 8?",
+      answer: "-2.2",
+    },
+    {
+      question: "Solve for x: 3x + 4 = 2x - 5",
+      answer: "-9",
+    },
+    {
+      question: "Find the value of x if 2(3x - 4) = 14x + 6",
+      answer: "2",
+    },
+    {
+      question: "Solve for x: 2x + 3(x - 4) = 9",
+      answer: "5",
+    },
+    {
+      question:
+        "What is the value of x in the equation 5(2x - 1) - 2(3x + 1) = -8?",
+      answer: "3/7",
+    },
+  ],
+};
+
+module.exports = questions;
