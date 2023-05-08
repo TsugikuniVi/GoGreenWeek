ca9b4fe8335980ce65943d356b418874690cae08
Saayuj Sajith <saayuj1015@gmail.com>
2023-05-08 21:13:58 +0400

    Updated Questions

--- questions.js
+++ questions.js
@@ -1,54 +1,87 @@
 const questions = {
   easy: [
     {
-      question: "What is the value of x in the equation 3x = 18?",
-      answer: "6",
-    },
-    {
-      question: "Solve for x: 5x - 7 = 18",
+      question: "What is the value of x in the equation 5x = 25?",
       answer: "5",
     },
     {
-      question: "Find the value of x if 2x + 3 = 11",
-      answer: "4",
+      question: "Solve for x: 3x + 4 = 19",
+      answer: "5",
     },
     {
-      question: "Solve for x: 8x - 12 = 20 + 2x",
+      question: "Find the value of x if 2x + 6 = 10",
       answer: "2",
     },
     {
-      question: "What is the value of x in the equation 7x - 5 = 12?",
-      answer: "17/7",
+      question: "Solve for x: 4x - 3 = 9",
+      answer: "3",
+    },
+    {
+      question: "What is the value of x in the equation 6x + 7 = 25?",
+      answer: "3",
+    },
+    {
+      question: "Solve for x: 2x + 5 = 13",
+      answer: "4",
+    },
+    {
+      question: "Find the value of x if 5x - 10 = 15",
+      answer: "5",
+    },
+    {
+      question: "Solve for x: 6x + 3 = 33",
+      answer: "5",
+    },
+    {
+      question: "What is the value of x in the equation 4x - 2 = 10?",
+      answer: "3",
+    },
+    {
+      question: "Solve for x: 7x - 4 = 45",
+      answer: "7",
     },
   ],
   medium: [
     {
-      question: "Solve for x: 2(x + 3) = 16",
-      answer: "5",
+      question: "Solve for x: 2(x + 4) = 24",
+      answer: "8",
     },
     {
-      question: "Find the value of x if 4x - 9 = 3x + 6",
-      answer: "15",
-    },
-    {
-      question: "Solve for x: 3(2x - 1) = 15",
+      question: "Find the value of x if 3x - 7 = 8x - 22",
       answer: "3",
     },
     {
-      question: "What is the value of x in the equation 5x + 7 = 3x - 1?",
-      answer: "-2",
+      question: "Solve for x: 5(2x - 1) = 15",
+      answer: "2",
     },
     {
-      question: "Solve for x: 2x + 5 = x - 7",
-      answer: "-12",
+      question: "What is the value of x in the equation 3x - 5 = 7x - 19?",
+      answer: "3",
     },
     {
-      question: "Find the value of x if 3(x + 2) = 27",
-      answer: "7",
+      question: "Solve for x: 2x + 7 = 3x - 4",
+      answer: "-11",
     },
     {
-      question: "Solve for x: 4x + 9 = 5(x - 2)",
-      answer: "23",
+      question: "Find the value of x if 4(x + 5) = 56",
+      answer: "6",
+    },
+    {
+      question: "Solve for x: 3(x - 2) = 27",
+      answer: "11",
+    },
+    {
+      question:
+        "What is the value of x in the equation 2(x + 3) - 3(x - 1) = 1?",
+      answer: "1/5",
+    },
+    {
+      question: "Solve for x: 2(3x - 1) = 4x + 7",
+      answer: "5",
+    },
+    {
+      question: "Find the value of x if 2x - 5 = 11 - 3x",
+      answer: "2",
     },
   ],
   hard: [
@@ -72,6 +105,47 @@ const questions = {
       question:
         "What is the value of x in the equation 5(2x - 1) - 2(3x + 1) = -8?",
       answer: "3/7",
+    },
+    {
+      question: "What is the value of x in the equation 4x + 3 = 3x + 7?",
+      answer: "4",
+    },
+    {
+      question: "Find the value of x if (x + 3)/5 - 2 = (3x - 1)/2",
+      answer: "11",
+    },
+    {
+      question: "Solve for x: 2x - 1 = x^2 - 6x + 5",
+      answer: "3",
+    },
+    {
+      question:
+        "What is the value of x in the equation (x + 2)/(2x - 1) = (x - 1)/(x + 3)?",
+      answer: "-1",
+    },
+    {
+      question: "Solve for x: 4x + 7 = 2(x^2 - 3)",
+      answer: "2, -1/2",
+    },
+    {
+      question: "Find the value of x if (x - 3)/4 - (x + 2)/2 = 1",
+      answer: "9",
+    },
+    {
+      question: "Solve for x: 3(x - 1) - 4(x + 2) = 5 - 2x",
+      answer: "-2",
+    },
+    {
+      question: "What is the value of x in the equation 2x - 1 = sqrt(3x + 4)?",
+      answer: "3",
+    },
+    {
+      question: "Solve for x: (x - 2)/(x + 3) + (x + 1)/(x - 4) = 2",
+      answer: "-5/3",
+    },
+    {
+      question: "Find the value of x if 4/x + 1 = 2/(x - 2)",
+      answer: "4",
     },
   ],
 };
